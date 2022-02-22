09e88908f6388da58790eaeb067f351f90e01b1f
jrochanav <98718621+jrochanav@users.noreply.github.com>
2022-02-22 13:36:17 -0600

    Create script.js

---
+++ mod4-solution/script.js
@@ -0,0 +1,10 @@
+(function()
+{var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (var name in names) {
+  if (name.charAt(0)== "J"||"j") {
+    byeSpeaker.speak;
+  } else {
+    helloSpeaker.speak;
+  }
+}})()
